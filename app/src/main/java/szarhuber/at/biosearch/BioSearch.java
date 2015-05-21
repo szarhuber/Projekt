ac0e9ed7f085f1eae24d124e1cb2ac4413739680
unknown <szarhuber@e32sws03.htl.grieskirchen.local>
2015-05-21 12:45:44 +0200

    <szarhuber>Added Detail-Class + it's XML-File</szarhuber>

--- app/src/main/java/szarhuber/at/biosearch/BioSearch.java
+++ app/src/main/java/szarhuber/at/biosearch/BioSearch.java
@@ -1,17 +1,31 @@
 package szarhuber.at.biosearch;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class BioSearch extends ActionBarActivity {
+public class BioSearch extends ActionBarActivity
+{
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bio_search);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetailClass.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
---
+++ app/src/main/java/szarhuber/at/biosearch/DetailClass.java
@@ -0,0 +1,16 @@
+package szarhuber.at.biosearch;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+/**
+ * Created by szarhuber on 21.05.2015.
+ */
+public class DetailClass extends ListActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_activity_);
+    }
+}

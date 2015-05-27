99447aed1e6eba342f56ac2159194dab53388480
szarhuber <simon.zrhuber@123434534534534.at>
2015-05-27 17:58:31 +0200

    Class + File
    Added MapViewClass and it's XML-File

---
+++ app/src/main/java/szarhuber/at/biosearch/MapViewActivity.java
@@ -0,0 +1,16 @@
+package szarhuber.at.biosearch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Manfred on 27.05.2015.
+ */
+public class MapViewActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity);
+    }
+}

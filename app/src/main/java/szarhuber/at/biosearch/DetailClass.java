f78bc11b7546cbdc9428bc7f66b7e676b1983254
szarhuber <szarhuber@1234.de>
2015-05-28 12:57:45 +0200

    Updated Detail class, with filtering PLZ.

--- app/src/main/java/szarhuber/at/biosearch/DetailClass.java
+++ app/src/main/java/szarhuber/at/biosearch/DetailClass.java
@@ -1,16 +1,42 @@
 package szarhuber.at.biosearch;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by szarhuber on 21.05.2015.
  */
 public class DetailClass extends ListActivity
 {
+    ArrayList<Markt> markets;
+    int plz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_activity_);
+        ListView lv = getListView();
+
+        Intent intent = getIntent();
+        plz = intent.getIntExtra("plz", 1);
+        switch (plz)
+        {
+            case 4676: markets = (ArrayList<Markt>)intent.getSerializableExtra("4676"); break;
+            case 4720: markets = (ArrayList<Markt>)intent.getSerializableExtra("4720"); break;
+
+            default: Log.i("SZARHUBER", "No available List");
+        }
+        displayItems();
+    }
+
+    public void displayItems()
+    {
+        ArrayAdapter<Markt> arrayAdapter = new ArrayAdapter<Markt>(this, android.R.layout.simple_list_item_1, markets);
+        setListAdapter(arrayAdapter);
     }
 }
